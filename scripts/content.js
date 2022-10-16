f2838c7338d6d8f1fd22b3b0b1383f9c68943087
ECK$TASY <chachel4188@gmail.com>
2022-10-16 10:19:53 -0500

    Initial commit

---
+++ scripts/content.js
@@ -0,0 +1,13 @@
+const login = document.getElementById("login");
+if (login) {
+  var text = "151997";
+  login.value = text;
+  console.log("done");
+}
+
+const password = document.getElementById("password");
+if (password) {
+  var text = "09062005";
+  password.value = text;
+  console.log("done");
+}
