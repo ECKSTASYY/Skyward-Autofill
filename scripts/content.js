7af797ed8e8de32d37036c3a9a561b1ba959ea6e
ECK$TASY <107089058+ECKSTASYY@users.noreply.github.com>
2022-12-22 10:43:07 -0600

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,5 @@
-
-  var useremail = ""; // UPDATE THIS PART WITH YOUR LOGIN EX; 15122
-  var userpassword = ""; // UPDATE THIS PART WITH YOUR PASSWORD EX: 090605
+const useremail = ""; // UPDATE THIS PART WITH YOUR LOGIN EX; 15122
+const userpassword = ""; // UPDATE THIS PART WITH YOUR PASSWORD EX: 090605
 
 
 const login = document.getElementById("login");
